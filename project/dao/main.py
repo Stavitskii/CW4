9dee64ccac54eb52bad78fbf2e2b92fcc454f5ff
Stavitskii <pochta100@mail.ru>
2022-08-16 17:38:50 +0300

    db created 36:30

--- project/dao/main.py
+++ project/dao/main.py
@@ -1,6 +1,19 @@
 from project.dao.base import BaseDAO
-from project.models import Genre
+from project.models import *
 
 
 class GenresDAO(BaseDAO[Genre]):
     __model__ = Genre
+
+
+class DirectorsDAO(BaseDAO[Director]):
+    __model__ = Director
+
+
+class MoviesDAO(BaseDAO[Movie]):
+    __model__ = Movie
+
+
+class UsersDAO(BaseDAO[User]):
+    __model__ = User
+
