d081ca9d05273753a62ecb5dc60dabfe08ebac6f
Stavitskii <pochta100@mail.ru>
2022-08-17 08:10:28 +0300

    api models ready, did not checked 01:01:30

--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -6,3 +6,29 @@ genre: Model = api.model('Жанр', {
     'id': fields.Integer(required=True, example=1),
     'name': fields.String(required=True, max_length=100, example='Комедия'),
 })
+
+director: Model = api.model('Режиссер', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Тейлор Шеридан'),
+})
+
+movie: Model = api.model('Фильм', {
+    'id': fields.Integer(required=True, example=1),
+    'title': fields.String(required=True, max_length=100, example='Йеллоустоун'),
+    'description': fields.String(required=True, max_length=250,
+                                 example='Владелец ранчо пытается сохранить землю своих предков. Кевин Костнер в неовестерне от автора «Ветреной реки»'),
+    'trailer': fields.String(required=True, max_length=100, example='https://www.youtube.com/watch?v=UKei_d0cbP4'),
+    'year': fields.Integer(required=True, example=2018),
+    'rating': fields.Float(required=True, example=8.6),
+    'genre_id': fields.Nested(genre),
+    'director_id': fields.Nested(director),
+})
+
+user: Model = api.model('Пользователь', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, max_length=100, example='mail@mail.ru'),
+    'password': fields.String(required=True, max_length=250, example='123456'),
+    'name': fields.String(required=False, max_length=100, example='Ivan'),
+    'surname': fields.String(required=False, max_length=100, example='Ivanov'),
+    'favourite_genre': fields.Nested(genre),
+})
