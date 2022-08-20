2100e79dbbcd018596166f429414b4dc8ba71c71
Stavitskii <pochta100@mail.ru>
2022-08-20 09:15:29 +0300

    02:09:07 делаем создание юзера в auth.py. Пока не проверял

--- project/views/auth/auth.py
+++ project/views/auth/auth.py
@@ -1,3 +1,34 @@
-from flask_restx import Namespace
+from flask_restx import Namespace, Resource
+
+from flask import request
+from project.container import user_service
+from project.setup.api.models import user
 
 api = Namespace('auth')
+
+
+@api.route('/register')
+class UsersRegisterView(Resource):
+
+    @api.marshal_with(user, as_list=True, code=200, description='OK')
+    def post(self):
+        """
+        Register user
+        """
+        data = request.json
+        if data.get('email') and data.get('password'):
+            return user_service.create_user()
+        else:
+            return "Email or password needed"
+
+
+@api.route('/login')
+class GenreView(Resource):
+    @api.response(404, 'Not Found')
+    @api.marshal_with(genre, code=200, description='OK')
+    def get(self, genre_id: int):
+        """
+        Get genre by id.
+        """
+        return genre_service.get_item(genre_id)
+
