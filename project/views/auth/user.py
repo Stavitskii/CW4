d05af92dc0eb3506d1b3cf7fb994de65d7c3a162
Stavitskii <pochta100@mail.ru>
2022-08-27 13:56:06 +0300

    03:45:12

--- project/views/auth/user.py
+++ project/views/auth/user.py
@@ -12,28 +12,30 @@ api = Namespace('user')
 @api.route('/')
 class RegisterView(Resource):
     @api.marshal_with(user, as_list=True, code=200, description='OK')
+    def patch(self):
+        data = request.json
+        header = request.headers
+        return user_service.update_user(data=data, refresh_token=header)
+
+
+
+    @api.marshal_with(user, as_list=True, code=200, description='OK')
     def get(self):
         data = request.json
         header = request.headers
-        user_service.update_user(data=data, refresh_token=header)
-
-        return "Something needed", 401
-
-    def patch(self):
-        data = request.json
-        if data.get('email') and data.get('password'):
-            return user_service.create_user(data.get('email'), data.get('password')), 201
-        else:
-            return "Something needed", 401
+        return user_service.get_user_by_token(refresh_token=header)
 
 
-@api.route('/password/')
-class LoginView(Resource):
-    @api.response(404, 'Not Found')
-    # @api.marshal_with(user, code=200, description='OK')
-    def post(self):
-        data = request.json
-        if data.get('email') and data.get('password'):
-            return user_service.check(data.get('email'), data.get('password')), 201
-        else:
-            return "Email or password needed", 401
+
+
+
+# @api.route('/password/')
+# class LoginView(Resource):
+#     @api.response(404, 'Not Found')
+#     # @api.marshal_with(user, code=200, description='OK')
+#     def post(self):
+#         data = request.json
+#         if data.get('email') and data.get('password'):
+#             return user_service.check(data.get('email'), data.get('password')), 201
+#         else:
+#             return "Email or password needed", 401
